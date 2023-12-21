f39a385122857b511a3a96192811ee5af2d256e5
Derrick L. Mayiku <derrickmal123@gmail.com>
2023-12-21 14:28:53 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -23,8 +23,8 @@ export default async ({ req, res, log, error }) => {
     log(`Account ID extracted: ${accountID}`);
 
     // Update the document in the database
-    await databases.updateDocument(DB_ID, "65706739032c0962d0a9", "6582b68644efb522bb73", {
-      accountStatus: "Active",
+    await databases.update document(DB_ID, STUD_COLLECTION_ID, "6582b68644efb522bb73", {
+      accountStatus: "Deleted",
     });
     log(`Account status updated for accountID: ${accountID}`);
 
