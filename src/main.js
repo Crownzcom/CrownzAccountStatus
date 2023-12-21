8cf0949049248d50dbde264ba2bd4be4b60a6e7d
Derrick L. Mayiku <derrickmal123@gmail.com>
2023-12-21 13:05:10 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,17 +5,29 @@ import { Client, Databases } from "node-appwrite";
 export default async ({ req, res, log, error }) => {
   try {
     //req params
-    log("1. ", req.bodyRaw); // Raw request body, contains request data
-    log("2. ", JSON.stringify(req.body)); // Object from parsed JSON request body, otherwise string
-    log("3. ", JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
-    log("4. ", req.scheme); // Value of the x-forwarded-proto header, usually http or https
-    log("5. ", req.method); // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
-    log("6. ", req.url); // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
-    log("7. ", req.host); // Hostname from the host header, such as awesome.appwrite.io
-    log("8. ", req.port); // Port from the host header, for example 8000
-    log("9. ", req.path); // Path part of URL, for example /v1/hooks
-    log("10. ", req.queryString); // Raw query params string. For example "limit=12&offset=50"
-    log("11. ", JSON.stringify(req.query)); // Parsed query params. For example, req.query.limit
+    log("1. " + req.bodyRaw); // Raw request body, contains request data
+    log("2. " + JSON.stringify(req.body)); // Object from parsed JSON request body, otherwise string
+    log("3. " + JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
+    log("4. " + req.scheme); // Value of the x-forwarded-proto header, usually http or https
+    log("5. " + req.method); // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
+    log("6. " + req.url); // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
+    log("7. " + req.host); // Hostname from the host header, such as awesome.appwrite.io
+    log("8. " + req.port); // Port from the host header, for example 8000
+    log("9. " + req.path); // Path part of URL, for example /v1/hooks
+    log("10. " + req.queryString); // Raw query params string. For example "limit=12&offset=50"
+    log("11. " + JSON.stringify(req.query)); // Parsed query params. For example, req.query.limit
+
+    log(req.bodyRaw); // Raw request body, contains request data
+    log(JSON.stringify(req.body)); // Object from parsed JSON request body, otherwise string
+    log(JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
+    log(req.scheme); // Value of the x-forwarded-proto header, usually http or https
+    log(req.method); // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
+    log(req.url); // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
+    log(req.host); // Hostname from the host header, such as awesome.appwrite.io
+    log(req.port); // Port from the host header, for example 8000
+    log(req.path); // Path part of URL, for example /v1/hooks
+    log(req.queryString); // Raw query params string. For example "limit=12&offset=50"
+    log(JSON.stringify(req.query)); // Parsed query params. For example, req.query.limit
 
     // Assuming the account ID is passed in the request body (You need to confirm the actual structure)
     // This might change based on actual event data structure
