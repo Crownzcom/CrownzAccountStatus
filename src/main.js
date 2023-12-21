5b3de977071831b2eac9c2f8824490241bcb7eb5
Derrick L. Mayiku <derrickmal123@gmail.com>
2023-12-21 11:45:28 +0300

    Update main.js
    updated with capability to update account status in kin account collection on deletion

--- src/main.js
+++ src/main.js
@@ -1,33 +1,36 @@
-import { Client } from 'node-appwrite';
+import { Client, Databases } from "node-appwrite";
 
 // This is your Appwrite function
-// It's executed each time we get a request
+// It's executed each time the function is triggered by an account deletion event
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  try {
+    // Initialize the Appwrite client
+    const client = new Client()
+      .setEndpoint("https://cloud.appwrite.io/v1") // Set your Appwrite endpoint
+      .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+      .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+    // Initialize the Appwrite database client
+    const databases = new Databases(client, "[DATABASE_ID]"); // Replace with your database ID
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+    // Assuming the account ID is passed in the request body (You need to confirm the actual structure)
+    const accountID = req.payload.$id; // This might change based on actual event data structure
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+    // Update the document in the database
+    await databases.updateDocument("65706739032c0962d0a9", accountID, {
+      accountStatus: "Deleted",
+    });
+    log(`Account status updated for accountID: ${accountID}`);
+
+    // Send a response back
+    return res.json({
+      message: `Account status updated successfully for accountID: ${accountID}`,
+    });
+  } catch (e) {
+    // Log and return error
+    error(`Error updating account status: ${e.message}`);
+    return res.json({
+      error: `Error updating account status: ${e.message}`,
+    });
   }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
 };
